5093401a63217e7e3c5a09906a76833eb593d294
Jay7735 <jaydeepdhillon@hotmail.co.uk>
2023-06-16 10:47:26 +0100

    should add one map with real world values -NW London Map - to seed-prod file

--- app/db/seedProd.js
+++ app/db/seedProd.js
@@ -91,7 +91,80 @@ set(ref(db), {
           LonDelta: 0,
           radius: 100,
         },
+      }, 103: {
+        mapName: 'NW London Hunt',
+        mapLocation: 'London ',
+        waypoints: [
+          {
+            title: 'Clue 1',
+            description: 'Sway',
+            Latitude: 51.55201314381003,
+            Longtitude: -0.19604067517332155,
+          },
+          {
+            title: 'Clue 2',
+            description: 'Pause thorny flower',
+            Latitude: 51.55169602340144,
+            Longtitude: -0.19228935889455837,
+          },
+          {
+            title: 'Clue 3',
+            description: 'Dark big cat...',
+            Latitude: 51.5514008942535,
+            Longtitude: -0.19194621751619295,
+          },
+          {
+            title: 'Clue 4',
+            description: 'Beautiful moon...',
+            Latitude: 51.550573558905796,
+            Longtitude: -0.19132867241574616,
+          },
+          {
+            title: 'Clue 5',
+            description: 'Speedy head covering...',
+            Latitude: 51.549614673429176,
+            Longtitude: -0.19144087592788794,
+          },
+          {
+            title: 'Clue 6',
+            description: 'Weird baskets...',
+            Latitude: 51.548992543956835,
+            Longtitude: -0.19125714460520324,
+          },
+          {
+            title: 'Clue 7',
+            description: 'Skint boy...',
+            Latitude: 51.548646005177936,
+            Longtitude: -0.19110971595580925,
+          },
+          {
+            title: 'Clue 8',
+            description: 'Little Mermaid St...',
+            Latitude: 51.547957765828116,
+            Longtitude: -0.19803297064039374,
+          },
+          {
+            title: 'Clue 9',
+            description: 'Hold me to your ear to hear the sea...',
+            Latitude: 51.54675346598928,
+            Longtitude: -0.20355016454798844,
+          },
+          {
+            title: 'Clue 10',
+            description: 'Fiery demise stopping place...',
+            Latitude: 51.54681851797777,
+            Longtitude: -0.20398334128926074,
+          }
+        ],
+        location: {
+          Latitude: 51.549657793651335,
+          Longtitude: -0.19767465976955112,
+          LatDelta: 0.04,
+          LonDelta: 0.05,
+          radius: 100,
+        },
       }
     },
     
 })
+
